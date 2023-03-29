35c8a8ce446f90c241e4fca8f1f0e56274e1a6fb
Abhishek kr Jha <Abhishekkjha068@outlook.com>
2023-03-29 22:06:58 +0530

    Namaste- Youtubee

---
+++ src/App.js
@@ -0,0 +1,35 @@
+
+import './App.css';
+import Head from './components/Head';
+import Body from './components/Body';
+
+function App() {
+  return (
+    <div>
+     
+      <Head/>
+      <Body/>
+
+      {
+        /****
+         * 
+         * Head
+         * Body
+         * Sidebar
+         *  -menuItems
+         * MainContainer
+         * ButtonList
+         * VideoContainer
+         * 
+         * Videocard
+         * 
+         * 
+         * 
+         */
+      }
+  
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Sidebar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div className='flex'>
+      sidebar
+    </div>
+  )
+}
+
+export default Sidebar
